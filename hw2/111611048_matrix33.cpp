27e73e9df376d5f0a2881bad66b017dcea0b1b79
Anthony1109 <chuchunchi.tw@gmail.com>
2024-07-22 00:11:45 +0800

    col fail edition

--- hw2/111611048_matrix33.cpp
+++ hw2/111611048_matrix33.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-matrix33::matrix33(vector3 v1, vector3 v2, vector3 v3) :v1(v1), v2(v2), v3(v3) {}
+matrix33::matrix33(vector3 v1, vector3 v2, vector3 v3) : col1(v1[0], v2[0], v3[0]), col2(v1[1], v2[1], v3[1]), col3(v1[2], v2[2], v3[2]) {}
 
-matrix33::matrix33(const matrix33& m): v1(m.v1), v2(m.v2), v3(m.v3) {}
+matrix33::matrix33(const matrix33& m): col1(m.col1), col2(m.col2), col3(m.col3) {}
 
 matrix33 matrix33::operator+(const matrix33& m){
     return matrix33(v1 + m.v1, v2 + m.v2, v3 + m.v3);
@@ -36,19 +36,14 @@ matrix33 operator-(const matrix33& m){
     return matrix33(-m.v1, -m.v2, -m.v3);
 }
 
-matrix33 matrix33::operator*(const matrix33& m1){
-    matrix33 result;  
-    for(int i = 0; i < 3; i++){
-        result[0][i] = v1[i]*m1[0][i] + v1[i]*m1[1][i] + v1[i]*m1[2][i];
-    }
-    for(int i = 0; i < 3; i++){
-        result[1][i] = v2[i]*m1[0][i] + v2[i]*m1[1][i] + v2[i]*m1[2][i];
-    }
-    for(int i = 0; i < 3; i++){
-        result[2][i] = v3[i]*m1[0][i] + v3[i]*m1[1][i] + v3[i]*m1[2][i];
-    }
-    
-    return result;
+matrix33 matrix33::operator*(const matrix33& m){ 
+    matrix33 result;
+        for (int i = 0; i < 3; ++i) {
+            result.v1[i] = v1[0] * m.v1[i] + v1[1] * m.v2[i] + v1[2] * m.v3[i];
+            result.v2[i] = v2[0] * m.v1[i] + v2[1] * m.v2[i] + v2[2] * m.v3[i];
+            result.v3[i] = v3[0] * m.v1[i] + v3[1] * m.v2[i] + v3[2] * m.v3[i];
+        }
+        return result;
 }
 
 bool matrix33::operator==(const matrix33& m){
@@ -88,9 +83,10 @@ matrix33& matrix33::operator/=(double k){
 }
 
 void matrix33::printMatrix(){
-    cout << v1[0] << " " << v2[0] << " " << v3[0] << endl;
-    cout << v1[1] << " " << v2[1] << " " << v3[1] << endl;
-    cout << v1[2] << " " << v2[2] << " " << v3[2] << endl;
+    col1.printVector3();
+    col2.printVector3();
+    col3.printVector3();
+    cout << endl;
 }
 
 matrix33 matrix33::invert(){
@@ -120,20 +116,20 @@ int matrix33::determinant(){
 
 vector3& matrix33::operator[](int index){
     if(index == 0)
-        return v1;
+        return col1;
     else if(index == 1)
-        return v2;
+        return col2;
     else
-        return v3;
+        return col3;
 }
 
 const vector3& matrix33::operator[](int index) const{
     if(index == 0)
-        return v1;
+        return col1;
     else if(index == 1)
-        return v2;
+        return col2;
     else
-        return v3;
+        return col3;
 }
 
 
